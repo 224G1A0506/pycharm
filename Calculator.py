bd9338b0474b9c19ca469feaaccb4b428ea3017c
Arif <224g1a0506@srit.ac.in>
2025-07-10 14:28:46 +0530

    hello

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,17 @@
 class Calculator:
-    def calc(self,a,b=0,c=0):
-        return a+b+c
+    def add(self,a,b):
+        return a+b
+    def sub(self,a,b):
+        return a-b
+    def mul(self,a,b):
+        return a*b
+    def div(self,a,b):
+        return a/b
 c=Calculator()
-print(c.calc(1,23))
+a=int(input())
+b=int(input())
+# print(c.add(a,b))
+print(f"Addition of ({a}, {b}) is {c.add(a, b)}")
+print(f"Subtraction of ({a},{b}) is {c.sub(a,b)}")
+print(f"Multiplication of ({a},{b}) is {c.mul(a,b)}")
+print(f"Division of ({a},{b}) is {c.div(a,b)}")
