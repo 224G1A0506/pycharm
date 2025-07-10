71046a4a91bee20bc79030e8c19539b4b339c80b
Arif <224g1a0506@srit.ac.in>
2025-07-10 15:37:38 +0530

    hello

--- StudentName.py
+++ StudentName.py
@@ -1,23 +1,30 @@
 class StudentName:
-    def __init__(self,name,marks):
-        self.__name=""
-        self.__marks=0
+    def __init__(self, name, marks):
+        self.__name = name
+        if 0 <= marks <= 100:
+            self.__marks = marks
+        else:
+            self.__marks = 0
+            print("Error: Invalid marks")
+
     def getName(self):
         return self.__name
-    def setName(self):
-        self.__name = name 
-    def getMarks(self):
-        return self.__marks
-    def setMarks(self):
-        if 0<=marks<=100:
-            self.__marks=marks
-        else:
-            print("error")
+
+    def setName(self, name):
+        self.__name = name
+
     def getMarks(self):
         return self.__marks
 
-c=StudentName("alice",100)
-c.setMarks()
+    def setMarks(self, marks):
+        if 0 <= marks <= 100:
+            self.__marks = marks
+        else:
+            print("Error: Invalid marks")
+
+
+c = StudentName("alice", 100)
+c.setMarks(95)
 c.setName("Alice")
 print(c.getName())
 print(c.getMarks())
